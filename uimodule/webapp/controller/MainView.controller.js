3aa02b33e2ccef19e0cd7ece99be944e4e4e4c48
Elisa <elisa.mani.em@gmail.com>
2021-04-23 10:03:19 +0000

    first commit

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,5 +1,16 @@
 sap.ui.define(["com/myorg/LogApp/controller/BaseController"], function (Controller) {
     "use strict";
 
-    return Controller.extend("com.myorg.LogApp.controller.MainView", {});
+    return Controller.extend("com.myorg.LogApp.controller.MainView", {
+      onLogout: function () {
+      //sap.m.URLHelper.redirect("/logout.html", false);
+      //window.location.replace('/do/logout');
+      window.location.href = '/logout';
+    },
+     onLogout2: function () {
+      //sap.m.URLHelper.redirect("/logout.html", false);
+      //window.location.replace('/do/logout');
+      window.location.replace('/logout');
+		}
+    });
 });
